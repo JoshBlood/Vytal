af642fc7b8d74bf6678a3a3d01aeee001d9e71a0
JoshBlood <98496339+JoshBlood@users.noreply.github.com>
2025-04-22 00:06:42 +0100

    Add files via upload

---
+++ Controllers/ModelsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VytalSign.Services;
+
+namespace VytalSign.Controllers
+{
+    public class ModelsController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public ModelsController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var models = await _apiService.GetHeartbeatsAsync();
+            return View(models);
+        }
+    }
+}
---
+++ Models/Heartbeat.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json.Serialization;
+namespace VytalSign.Models;
+public class Heartbeat
+{
+    [JsonPropertyName("deviceid")]
+    public int DeviceId { get; set; }
+
+    [JsonPropertyName("devicename")]
+    public string DeviceName { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("devicetypeid")]
+    public string DeviceTypeId { get; set; }
+
+    [JsonPropertyName("customerid")]
+    public string CustomerId { get; set; }
+
+    [JsonPropertyName("customerkey")]
+    public string CustomerKey { get; set; }
+
+    [JsonPropertyName("customername")]
+    public string CustomerName { get; set; }
+
+    [JsonPropertyName("dt")]
+    public string Dt { get; set; } 
+
+    [JsonPropertyName("val")]
+    public double Val { get; set; }  
+
+    [JsonPropertyName("lat")]
+    public double Lat { get; set; }
+
+    [JsonPropertyName("lng")]
+    public double Lng { get; set; }
+
+    [JsonPropertyName("unit")]
+    public string Unit { get; set; }
+
+    [JsonPropertyName("descriptiondevicetype")]
+    public string DescriptionDeviceType { get; set; }
+
+    [JsonPropertyName("groupid")]
+    public string GroupId { get; set; }
+
+    [JsonPropertyName("locationid")]
+    public int LocationId { get; set; }
+
+    [JsonPropertyName("deleted")]
+    public bool Deleted { get; set; }
+
+    [JsonPropertyName("lastupdate")]
+    public long LastUpdate { get; set; }
+
+    [JsonPropertyName("online")]
+    public bool Online { get; set; }
+}
